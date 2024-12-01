b15a2469ac34d9e20f15860d06609f871e84ef88
Parth Vaidya <zeusparth99@gmail.com>
2024-12-01 22:14:33 +0530

    Updated the code to test

--- Snake Multiplayer/Assets/Scripts/RandomizedPositionUtility.cs
+++ Snake Multiplayer/Assets/Scripts/RandomizedPositionUtility.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RandomizedPositionUtility : MonoBehaviour
 {
     public static Vector3 GetRandomPosition(BoxCollider2D gridArea)
-    {
+    { //added randomization
+
         Bounds bounds = gridArea.bounds;
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
